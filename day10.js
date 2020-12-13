4e77c7e53807df5679b01cd3b34cd3c28f5c9374
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-12 21:33:08 -0500

    day 10 part 1

---
+++ day10.js
@@ -0,0 +1,33 @@
+const input = require("fs").readFileSync("./new.txt", "utf-8").trim().split('\r\n').map(Number);
+
+function part1(){
+  input.sort( function(a,b) { return a - b; } );
+  let ones = 0;
+  let threes = 1;
+  // console.log(input);
+  // count difference between first value and original value of 0
+  if(input[0] === 1){
+    ones++;
+  }
+  else{
+    if(input[0] === 3){
+      threes++
+    }
+  }
+  for(let i = 0; i < input.length - 1; i++){
+    if(input[i+1] - input[i] === 1){
+    //   console.log ('one: ', input[i+1], ' ', input[i]);
+      ones++;
+    }
+    else{if(input[i+1] - input[i] === 3){
+    //   console.log ('three: ', input[i+1], ' ', input[i]);
+      threes++;
+    }
+    else{
+    //   console.log ('none: ', input[i+1], ' ', input[i]);
+    }
+  }}
+  console.log('ones: ', ones, ' threes: ', threes, 'answer: ', ones*threes);
+}
+
+part1();
