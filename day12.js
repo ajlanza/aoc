f47b41142bbd77a144a542aef1b04c4b85cc9a5a
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-14 20:13:38 -0500

    day12 part 1 solved

--- day12.js
+++ day12.js
@@ -1,75 +1,61 @@
-const input = require("fs").readFileSync("./new.txt", "utf-8").trim().split('\r\n');
+const input = require("fs").readFileSync("./new.txt", "utf-8").trim().split('\n');
 
 function part1() {
   let facing = 90;
   let x = 0;
   let y = 0;
   for(let i = 0; i < input.length; i++){
-      console.log('i ', i);
     action = input[i].charAt(0);
     value = parseInt(input[i].slice(1));
-    
-    if(action === 'N'){
-      y += value;
-      console.log('North ', value);
-      continue;
-    }
-    if(action === 'S'){
-      y -= value;
-      console.log('South ', value)
-      continue;
-    }
-    if(action === 'E'){
-      x += value;
-      console.log('East ', value)
-      continue;
-    }
-    if(action === 'W'){
-      y -= value;
-      console.log('West ', value)
-      continue;
-    }
-    if(action === 'R'){
-      facing += value;
-      if(facing > 360){
-        facing = facing - 360;
-      }
-      console.log('Right ', value, 'Facing: ', facing)
-    }
-    if(action === 'L'){
-      facing -= value;
-      if(facing <= 0){
-        facing = 360 - facing;
-      }
-      console.log('Left ', value, 'Facing: ', facing)
-    }
-    if(action === 'F'){
-      // 90 degrees is East
-      if(facing === 90){
-        x += value;
-        console.log('Forward East ', value);
-        continue;
-      }
-      // 180 degrees is South
-      if(facing === 180){
-        y -= value;
-        console.log('Forward South ', value);
-        continue;
-      }
-      // 270 degrees is West
-      if(facing === 270){
-        x -= value;
-        console.log('Forward West ', value);
-        continue;
-      }
-      // 360 degrees is North
-      if(facing === 360 || facing === 0){
+    switch(action) {
+      case 'N':
         y += value;
-        console.log('Forward North ', value);
-        continue;
-      }
+        break;
+      case 'S':
+        y -= value;
+        break;
+      case 'E':
+        x += value;
+        break;
+      case 'W':
+        x -= value;
+        break;
+      case 'R':
+        facing += value;
+        if(facing > 360){
+          facing = facing - 360;
+        }
+        break;
+      case 'L':
+        facing -= value;
+        if(facing <= 0){
+        // This was my problem. since facing is negative we add it. Subtracting the negative number was acting as addition.
+          facing = 360 + facing;
+        }
+        break;
+      case 'F':
+        // 90 degrees is East
+        if(facing === 90){
+          x += value;
+        }
+        // 180 degrees is South
+        if(facing === 180){
+          y -= value;
+        }
+        // 270 degrees is West
+        if(facing === 270){
+          x -= value;
+        }
+        // 360 degrees is North
+        if(facing === 0 || facing === 360){
+          y += value;
+        }
+        break;
+      default: 
+        console.log('error');        
     }
   }
+
   console.log('X axis: ', x, 'Y axis: ', y);
   if(x < 0){
     x = x * -1;
@@ -77,7 +63,7 @@ function part1() {
   if(y < 0){
     y = y * -1;
   }
-  console.log(x, y, x + y);
+  console.log('Manhattan distance: ', x + y);
 }
 
 part1();
