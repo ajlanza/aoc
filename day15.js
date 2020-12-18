7f680520cf6ca68e12d168fa415dc0d91df3bb88
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-17 20:19:20 -0500

    day 15 part 2

--- day15.js
+++ day15.js
@@ -24,4 +24,37 @@ function part1(){
   console.log(Object.keys(numbers).find(key => numbers[key] === 2020));
 }
 
+function part2(){
+    let numbers={
+      1: 1,
+      17: 2,
+      0: 3,
+      10: 4,
+      18: 5,
+      11: 6,
+      6: 7
+    }
+    let thisTurn = 0;
+    let temp = 0;
+    const turns = 30000000
+    for(let i = 8; i <= turns; i++){
+      // just to show that it is running
+      if(i % 1000 === 0){
+       console.log(i);
+      }
+      if(numbers.hasOwnProperty(thisTurn)){
+        temp = thisTurn;
+        thisTurn = i - numbers[thisTurn];
+        numbers[temp] = i;
+      }
+      else{
+        numbers[thisTurn] = i;
+        thisTurn = 0;
+      }
+    }
+    console.log(Object.keys(numbers).find(key => numbers[key] === 30000000));
+  }
+
+
 part1();
+part2();
