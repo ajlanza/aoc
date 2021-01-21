b0158c2af7722027b68ece1b7adec574f07a6793
ajlanza <61472953+ajlanza@users.noreply.github.com>
2021-01-20 21:45:47 -0500

    Day 3 solved

--- 2015/day3/day3.js
+++ 2015/day3/day3.js
@@ -39,4 +39,76 @@ part1 = () => {
   console.log('Unique houses: ', uniqueHouses);
 }
 
-part1();
+part2 = () => {
+    let grid = {};
+    let NorthSouth = 0;
+    let EastWest = 0;
+    let RoboNS = 0;
+    let RoboEW = 0;
+    let uniqueHouses = 1;
+    grid[NorthSouth] = {};
+    grid[NorthSouth][EastWest] = 1;
+    for(let i = 0; i < input.length; i++) {
+      if(i % 2 === 0){
+        switch(input.charAt(i)) {
+          case '^':
+              NorthSouth++;
+              break;
+          case '>':
+              EastWest++;
+              break;
+          case 'v':
+              NorthSouth--;
+              break;
+          case '<':
+              EastWest--;
+              break;
+        }
+        if(!grid[NorthSouth]) {
+          grid[NorthSouth] = {};
+          grid[NorthSouth][EastWest] = 1;
+          uniqueHouses++;
+          continue;
+        }
+        if(!grid[NorthSouth][EastWest]) {
+          grid[NorthSouth][EastWest] = 1;
+          uniqueHouses++
+        }
+        else {
+          grid[NorthSouth][EastWest]++
+        }
+      }
+      else {
+        switch(input.charAt(i)) {
+          case '^':
+              RoboNS++;
+              break;
+          case '>':
+              RoboEW++;
+              break;
+          case 'v':
+              RoboNS--;
+              break;
+          case '<':
+              RoboEW--;
+              break;
+        }
+        if(!grid[RoboNS]) {
+          grid[RoboNS] = {};
+          grid[RoboNS][RoboEW] = 1;
+          uniqueHouses++;
+          continue;
+        }
+        if(!grid[RoboNS][RoboEW]) {
+          grid[RoboNS][RoboEW] = 1;
+          uniqueHouses++
+        }
+        else {
+          grid[RoboNS][RoboEW]++
+        }
+      }
+    }
+    console.log('Unique houses: ', uniqueHouses);
+  }
+
+part2();
