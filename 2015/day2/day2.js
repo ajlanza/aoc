6ba7cfe3d05dcff3beefd8d80cdaae45399d7af2
ajlanza <61472953+ajlanza@users.noreply.github.com>
2021-01-20 20:30:30 -0500

    Day 2 solved

--- 2015/day2/day2.js
+++ 2015/day2/day2.js
@@ -47,4 +47,17 @@ part1 = () => {
     return (console.log(`The elves need ${totalPaperNeeded} sqft of paper.`));
 }
 
+part2 = () => {
+    let ribbon = 0;
+    for(let i = 0; i < input.length; i++){
+      const box = input[i].split('x');
+      box.sort(function(a, b){return a-b})
+      const smallestPerimeter = 2 * (parseInt(box[0]) + parseInt(box[1]));
+      const bow = box[0] * box[1] * box[2];
+      ribbon += smallestPerimeter + bow;
+    }
+    return(console.log(`The Elves need ${ribbon} feet of ribbon!`));
+}
+
 part1();
+part2();
