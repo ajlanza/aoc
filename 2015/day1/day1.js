d1b5f0a08151e8337107c6255176626c7497ed67
ajlanza <61472953+ajlanza@users.noreply.github.com>
2021-01-20 19:25:55 -0500

    Day 1 part 2 solved

--- 2015/day1/day1.js
+++ 2015/day1/day1.js
@@ -13,4 +13,20 @@ part1 = () => {
   console.log("Santa needs to get to floor ", floor);
 }
 
+part2 = () => {
+    let floor = 0;
+    for(let i = 0; i < input.length; i++){
+      if(input.charAt(i) === ("(")) {
+          floor++;
+      }
+      else {
+          floor--;
+          if(floor === -1) {
+            return (console.log("Santa went into the basement at position", i+1));
+          }
+      }
+    }    
+  }
+
 part1();
+part2();
