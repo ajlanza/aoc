740e3e7fb044fb85ba1c7a409f40fe20b6ca5070
ajlanza <61472953+ajlanza@users.noreply.github.com>
2021-01-24 17:41:20 -0500

    Day 8 part 1

---
+++ 2015/day8/day8.js
@@ -0,0 +1,27 @@
+const list = require("fs").readFileSync("./inputDay8.txt", "utf-8").split('\r\n');
+
+part1 = () => {
+  let charactersOfCode = 0;
+  let stringCharacters = 0;
+  for(let i = 0; i < list.length; i++){
+    let line = list[i];
+    let escapedCharacters = 0;
+    charactersOfCode += line.length;
+    for(let j = 0; j < line.length; j++){
+      if(line[j] === '\\'){
+          if(line[j+1] === '\\' || line[j+1] === '\"'){
+            escapedCharacters += 1;
+            j++;
+          }
+          else if(line[j+1] === 'x'){
+            escapedCharacters += 3;
+            j += 3;
+          }
+      }
+    }
+    stringCharacters += line.length - escapedCharacters -2;
+  }
+  console.log(charactersOfCode - stringCharacters);
+}
+
+part1();
